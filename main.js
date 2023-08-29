ab46422cb1fa426f39b2e6fa00a95303366d9213
CncAnon <cncanon1@proton.me>
2023-08-29 13:03:11 +0300

    Change body limit from default, show help with context

--- main.js
+++ main.js
@@ -3,7 +3,9 @@ require("dotenv").config();
 const express = require('express');
 const axios = require('axios');
 const app = express();
-app.use(express.json());
+const bodyParser = require('body-parser');
+app.use(bodyParser.json({ limit: '100mb' }));
+app.use(bodyParser.urlencoded({limit: '100mb', extended: true}));
 
 const API_TOKEN = process.env.API_TOKEN;
 
